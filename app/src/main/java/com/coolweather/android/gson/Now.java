5520671452ec81df699620681045060535e5d856
kaffay <kaffay@qq.com>
2018-05-21 11:39:48 +0800

    layout files

---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by kaff on 2018-05-21.
+ */
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+    public class More {
+        @SerializedName("txt")
+        public String info;
+    }
+}
